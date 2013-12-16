0243408440ff22d6bebb8b4dad1f3a868cd62a11
BerliozLeChat <neoflash93@gmail.com>
2013-12-16 18:00:10 +0100

    Ajout de C2

---
+++ src/C2/cartouchiere.h
@@ -0,0 +1,15 @@
+/* 
+ * File:   cartouchiere.h
+ * Author: didierrobbes
+ *
+ * Created on 7 novembre 2013, 16:06
+ */
+
+#ifndef CARTOUCHIERE_H
+#define	CARTOUCHIERE_H
+
+// le type T_ELT doit etre defini avant 
+typedef ;
+
+#endif	/* CARTOUCHIERE_H */
+
